84c4bed1c10f0041c158e55cef927aed85c7c86c
nobody <nobody@5f49af62-2c63-493f-87c0-414fe00f4c69>
2006-11-28 19:49:42 +0000

    Add missing control-shell test & examples directories

---
+++ source/gbbopen/control-shells/examples/abort-ks-execution.lisp
@@ -0,0 +1,91 @@
+;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-USER; Syntax:common-lisp -*-
+;;;; *-* File: /home/gbbopen/current/source/gbbopen/control-shells/examples/abort-ks-execution.lisp *-*
+;;;; *-* Edited-By: cork *-*
+;;;; *-* Last-Edit: Wed Nov 15 12:25:55 2006 *-*
+;;;; *-* Machine: ruby.corkills.org *-*
+
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;; *
+;;;; *                       Abort KS Execution Example
+;;;; *
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;
+;;; Written by: Dan Corkill
+;;;
+;;; Copyright (C) 2006, Dan Corkill <corkill@GBBopen.org>
+;;; Part of the GBBopen Project (see LICENSE for license information).
+;;;
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;;;
+;;;  11-13-06 File Created.  (Corkill)
+;;;
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+(in-package :gbbopen-user)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package '(:portable-threads :agenda-shell)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (import '(common-lisp-user::*autorun-gbbopen-modules*)))
+
+;;; ---------------------------------------------------------------------------
+
+(define-ks ks-to-abort 
+    :trigger-events ((start-control-shell-event)) 
+    :execution-function 'ks-to-abort-execution-function)
+
+(defun ks-to-abort-execution-function (ksa)
+  (declare (ignore ksa))
+  (format t "~&;; KS ~s is going to sleep...~%"
+          'ks-to-abort)
+  (polling-sleep 5)
+  (warn "KS ~s was not aborted." 'ks-to-abort))
+
+;;; ---------------------------------------------------------------------------
+
+(defun initializations (event-name &key &allow-other-keys)
+  (declare (ignore event-name))
+  ;; Clean up any previous run:
+  (reset-gbbopen :retain-classes '((ks :plus-subclasses))
+                 :retain-event-functions 't
+                 :retain-event-printing 't)
+  (let ((sleep-time 2))
+    ;; Got multiprocessing? Spawn a process to abort ths KS execution:
+    #-multiprocessing-not-available
+    (spawn-process "KS aborter" 
+                   #'(lambda (sleep-time)
+                       (format t "~&;; Spawned aborting process, sleeping ~
+                                   for ~s second~:p...~%"
+                               sleep-time)                       
+                       (sleep sleep-time)
+                       (format t "~&;; Aborting KS execution...~%")
+                       (abort-ks-execution))
+                   sleep-time)
+    ;; No multiprocessing? Set the abort-time and add a polling-function to
+    ;; abort the KS execution:
+    #+multiprocessing-not-available
+    (let ((abort-time (+ (get-universal-time) sleep-time)))
+      (labels ((abort-ks-polling-function ()
+                 (when (>= (get-universal-time) abort-time)
+                   (remove-polling-function #'abort-ks-polling-function)
+                   (format t "~&;; Aborting KS execution...~%")
+                   (abort-ks-execution))))
+        (add-polling-function #'abort-ks-polling-function)))))
+
+(add-event-function 'initializations 'start-control-shell-event
+                    ;; Initializations should be done first!
+                    :priority 100)
+
+;;; ---------------------------------------------------------------------------
+
+(when *autorun-gbbopen-modules*
+  (format t "~{~&~s~%~}"
+	  (multiple-value-list (start-control-shell))))
+
+;;; ===========================================================================
+;;;				  End of File
+;;; ===========================================================================
+
---
+++ source/gbbopen/control-shells/test/agenda-shell-test.lisp
@@ -0,0 +1,234 @@
+;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-USER; Syntax:common-lisp -*-
+;;;; *-* File: /home/gbbopen/current/source/gbbopen/control-shells/test/agenda-shell-test.lisp *-*
+;;;; *-* Edited-By: cork *-*
+;;;; *-* Last-Edit: Fri Nov 10 06:40:18 2006 *-*
+;;;; *-* Machine: ruby.corkills.org *-*
+
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;; *
+;;;; *                     Simple Agenda-Shell Trip Test
+;;;; *
+;;;; **************************************************************************
+;;;; **************************************************************************
+;;;
+;;; Written by: Dan Corkill
+;;;
+;;; Copyright (C) 2004-2006, Dan Corkill <corkill@GBBopen.org>
+;;; Part of the GBBopen Project (see LICENSE for license information).
+;;;
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+;;;
+;;;  04-21-04 File Created.  (Corkill)
+;;;  10-05-06 Added define-ks-class and define-ksa-class tests.  (Corkill)
+;;;
+;;; * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+(in-package :gbbopen-user)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (use-package '(:agenda-shell)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (import '(common-lisp-user::*autorun-gbbopen-modules*
+            agenda-shell::*cs*)))
+
+;;; ===========================================================================
+;;;   Unit definitions
+
+(define-unit-class uc-one ()
+  ((value :initform 0)
+   (uc-two :link (uc-two uc-one :singular t)
+	 :singular t))
+  (:dimensional-values
+   (value :point value))
+  (:initial-space-instances (space-1)))
+
+;;; ---------------------------------------------------------------------------
+
+(define-unit-class uc-two ()
+  ((value)
+   (uc-one :link (uc-one uc-two :singular t)
+	 :singular t))
+  (:dimensional-values
+   (value :point value))
+  (:initial-space-instances (space-2)))
+
+;;; ---------------------------------------------------------------------------
+;;;  A user-defined KS and KSA class for testing purposes:
+
+(define-ks-class obviated-ks ()
+  ((an-unused-slot :initform nil)))
+
+(define-ksa-class retriggered-ksa ()
+  ((rating :documentation "Simply shadowing this slot")))
+
+;;; ===========================================================================
+;;;   KS definitions
+
+(define-ks start-control-shell-ks
+    :trigger-events ((start-control-shell-event))
+    :activation-predicate 
+    #'(lambda (ks event)
+        (declare (ignore ks event))
+        (format t "~&;; Start-control-shell-ks ~
+                        :activation-predicate executed~%")
+        t)
+    :revalidation-predicate 
+    #'(lambda (ksa)
+        (declare (ignore ksa))
+        (format t "~&;; Start-control-shell-ks ~
+                        :revalidation-predicate executed~%")
+        t)
+    :precondition-function 
+    #'(lambda (ks events)
+        (declare (ignore ks events))
+        (format t "~&;; Start-control-shell-ks ~
+                        :precondition-function executed~%")
+        most-positive-rating)
+    :execution-function 
+    #'(lambda (ksa)
+	(let ((trigger-events (trigger-events-of ksa)))
+	  (unless (and (consp trigger-events)
+		       (= (length trigger-events) 1)
+		       (typep (first trigger-events)
+			      'start-control-shell-event))
+	    (error "Start-control-shell: Wrong events ~s" 
+		   trigger-events)))
+        (format t "~&;; Start-control-shell-ks ~
+                        :execution-function executed~%")
+        (values)))
+
+;;; ---------------------------------------------------------------------------
+
+(define-ks quiescence-event-ks
+  :trigger-events ((quiescence-event))
+  :execution-function 
+  #'(lambda (ksa)
+      (format t "~&;; Quiescense ks executed~%")
+      ;; disable this KS after 1 use!
+      (setf (ks-enabled-p (ks-of ksa)) nil)
+      (make-instance 'uc-one)
+      (values)))
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks retriggered-event-ks
+  :trigger-events ((create-instance-event uc-one))
+  :retrigger-events ((create-instance-event uc-two))
+  :rating -1
+  :retrigger-function
+  #'(lambda (ksa event)
+      (declare (ignore event))
+      (format t "~&;; Retriggered KS retriggered~%")
+      (setf (rating-of ksa) 100)
+      ;; Check that this ksa is now first on the pending-ksas queue:
+      (unless (eq ksa (select-ksa-to-execute *cs*))
+        (error "KSA rating did not effect the pending-ksas queue")))
+  :execution-function 
+  #'(lambda (ksa)
+      (declare (ignore ksa))
+      (format t "~&;; Retriggered KS executed~%"))
+  :ksa-class retriggered-ksa)
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks obviation-event-ks
+  :trigger-events ((create-instance-event uc-one))
+  :obviation-events ((quiescence-event))
+  :rating -1
+  :obviation-predicate 
+  #'(lambda (ksa event)
+      (declare (ignore ksa event))
+      (format t "~&;; Obviation KS obviated~%")      
+      't)
+  :execution-function 
+  #'(lambda (ksa)
+      (error "Obviation KS ~s was not obviated" ksa))
+  :ks-class obviated-ks)
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks create-uc-one-instance-event-ks
+  :trigger-events ((create-instance-event uc-one))
+  :execution-function 
+  #'(lambda (ksa)
+      (format t "~&;; Create-uc-one-instance-event ks executed~%")
+      (let ((trigger-instance (sole-trigger-instance ksa)))
+	(make-instance 'uc-two :uc-one trigger-instance))
+      (values)))
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks add-uc-one-to-space-instance-event-ks
+    :trigger-events ((add-instance-to-space-instance-event uc-one :path (*)))
+    :execution-function 
+    #'(lambda (ksa)
+	(declare (ignore ksa))
+	(format t "~&;; Add-uc-one-to-space-instance-event ks executed~%")
+	(values)))
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks update-uc-one-value-slot-event-ks
+  :trigger-events ((update-nonlink-slot-event uc-one :slot-name value))
+  :execution-function 
+  #'(lambda (ksa)
+      (declare (ignore ksa))
+      (format t "~&;; Update-uc-one-value-slot-event ks executed~%")
+      (values)))
+		   
+;;; ---------------------------------------------------------------------------
+
+(define-ks link-uc-two-event-ks
+  :trigger-events ((link-event uc-two :slot-name uc-one))
+  :execution-function 
+  #'(lambda (ksa)
+      (declare (ignore ksa))
+      (format t "~&;; Link-uc-two-event ks executed~%")
+      (values)))
+		   
+;;; ===========================================================================
+;;;   Initializations (run at Agenda Shell startup)
+
+(defun initializations (event-name &key &allow-other-keys)
+  (declare (ignore event-name))
+  ;; Clean up any previous run:
+  (reset-gbbopen :retain-classes '((ks :plus-subclasses))
+		 :retain-event-functions 't)
+  ;; Enable all KSs:
+  (map-instances-of-class #'(lambda (ks) (setf (ks-enabled-p ks) 't)) 'ks)
+  ;; Make new space instances:
+  (make-space-instance '(space-1)
+		       :dimensions '((value :ordered)))
+  (make-space-instance '(space-2)
+		       :dimensions '((value :ordered)))
+  #+when-desired
+  (enable-event-printing '(ksa-event :plus-subevents) 'ksa))
+
+(add-event-function 'initializations 'start-control-shell-event
+		    ;; Initializations should be done first!
+		    :priority 100)
+
+;;; ===========================================================================
+;;;   Agenda Shell test
+
+(defun agenda-shell-test (&rest initargs)
+  (declare (dynamic-extent initargs))
+  (format t "~&;;; Starting Agenda Shell Test....~%")
+  (multiple-value-prog1 
+      (apply #'start-control-shell initargs)
+    (format t "~&;;; Agenda Shell Test Completed~%")))
+
+;;; ---------------------------------------------------------------------------
+
+(when *autorun-gbbopen-modules*
+  (format t "~{~&~s~%~}"
+	  (multiple-value-list 
+	   (agenda-shell-test :save-executed-ksas 't))))
+
+;;; ===========================================================================
+;;;				  End of File
+;;; ===========================================================================
+
+
