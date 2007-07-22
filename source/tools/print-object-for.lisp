ee612d6dfb13b4963d26c786114be5ce9da4e3f6
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2007-07-22 03:52:56 +0000

    Add CLOS instance and hash table support

--- source/tools/print-object-for.lisp
+++ source/tools/print-object-for.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-TOOLS; Syntax:common-lisp -*-
 ;;;; *-* File: /home/gbbopen/current/source/tools/print-object-for.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Sat Jul 21 16:45:12 2007 *-*
+;;;; *-* Last-Edit: Sat Jul 21 23:49:41 2007 *-*
 ;;;; *-* Machine: ruby.corkills.org *-*
 
 ;;;; **************************************************************************
@@ -26,6 +26,10 @@
 (in-package :gbbopen-tools)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
+  (import '(clos:class-slots
+            clos:slot-definition-name)))
+  
+(eval-when (:compile-toplevel :load-toplevel :execute)
   (export '(print-object-for-saving     ; not yet documented
             print-object-for-sending))) ; not yet documented
 
@@ -35,6 +39,7 @@
 (defgeneric print-object-for-sending (object stream))
 
 ;;; ---------------------------------------------------------------------------
+;;;  Default
 
 (macrolet ((make (name)
              `(defmethod ,name (object stream)
@@ -43,6 +48,7 @@
   (make print-object-for-sending))
 
 ;;; ---------------------------------------------------------------------------
+;;;  Lists
 
 (macrolet ((make (name)
              `(defmethod ,name ((cons cons) stream)
@@ -70,6 +76,7 @@
   (make print-object-for-sending))
 
 ;;; ---------------------------------------------------------------------------
+;;;  Vectors
 
 (macrolet ((make (name)
              `(defmethod ,name ((vector vector) stream)
@@ -84,6 +91,7 @@
   (make print-object-for-sending))
 
 ;;; ---------------------------------------------------------------------------
+;;;  Arrays
 
 (macrolet ((make (name)
              `(defmethod ,name ((array array) stream)
@@ -108,6 +116,60 @@
                     (helper dimensions)
                     (princ " " stream)))
                 array)))
+  (make print-object-for-saving)
+  (make print-object-for-sending))
+
+;;; ---------------------------------------------------------------------------
+;;;  Structures
+
+(macrolet ((make (name)
+             `(defmethod ,name ((structure structure-object) stream)
+                (let ((class (class-of structure)))
+                  (format stream "#S(~s" (class-name class))
+                  (dolist (slot (class-slots class))
+                    (let ((slot-name (slot-definition-name slot)))
+                      (format stream " :~a " slot-name)
+                      (,name (slot-value structure slot-name) stream)))
+                  (princ ")" stream))
+                structure)))
+  (make print-object-for-saving)
+  (make print-object-for-sending))
+
+;;; ---------------------------------------------------------------------------
+;;;  Instances
+
+(macrolet ((make (name)
+             `(defmethod ,name ((instance standard-object) stream)
+                (let ((class (class-of instance)))
+                  (format stream "#I(~s" (class-name class))
+                  (dolist (slot (class-slots class))
+                    (let ((slot-name (slot-definition-name slot)))
+                      (format stream " :~a " slot-name)
+                      (if (slot-boundp instance slot-name)
+                          (,name (slot-value instance slot-name) stream)
+                          ;; Unbound value indicator:
+                          (format stream "#~"))))
+                  (princ ")" stream))
+                instance)))
+  (make print-object-for-saving)
+  (make print-object-for-sending))
+
+;;; ---------------------------------------------------------------------------
+;;;  Hash Tables
+
+(macrolet ((make (name)
+             `(defmethod ,name ((hash-table hash-table) stream)
+                (format stream "#H(~s ~s"
+                        (hash-table-test hash-table)
+                        (hash-table-count hash-table))
+                (maphash #'(lambda (key value)
+                             (format stream " ")
+                             (,name key stream)
+                             (format stream " ")
+                             (,name value stream))
+                         hash-table)
+                (princ ")" stream)
+                hash-table)))
   (make print-object-for-saving)
   (make print-object-for-sending))
 
