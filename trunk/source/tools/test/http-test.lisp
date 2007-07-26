e4cca35497b183a7be7eb648de46f7d0989d4d33
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2007-07-26 06:02:13 +0000

    Add more socket open/close/read/write/shutdown testing

--- trunk/source/tools/test/http-test.lisp
+++ trunk/source/tools/test/http-test.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:PORTABLE-SOCKETS-USER; Syntax:common-lisp -*-
 ;;;; *-* File: /home/gbbopen/current/source/tools/test/http-test.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Sat Jul 14 13:44:01 2007 *-*
+;;;; *-* Last-Edit: Wed Jul 25 17:46:34 2007 *-*
 ;;;; *-* Machine: ruby.corkills.org *-*
 
 ;;;; **************************************************************************
@@ -32,6 +32,27 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (import '(common-lisp-user::*autorun-modules*)))
+
+;;; ---------------------------------------------------------------------------
+
+(defparameter *test-port* 9876)
+
+;;; ---------------------------------------------------------------------------
+
+(defun log-error (control-string &rest args)
+  (declare (dynamic-extent args))
+  (format t "~&;; ~73,,,'*<*~>~
+             ~%;; *** ~?~
+             ~&;; ~73,,,'*<*~>"
+          control-string
+          args))
+
+;;; ---------------------------------------------------------------------------
+
+(defun forced-format (&rest args)
+  (declare (dynamic-extent args))
+  (apply #'format t args)
+  (force-output))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -66,7 +87,51 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(when *autorun-modules* (http-test "GBBopen.org" 80))
+(defun passive-socket-tests ()
+  (forced-format "~&;; Performing passive-socket and connection tests...")
+  (let ((passive-socket (make-passive-socket *test-port*))
+        (client1 (open-connection *localhost* *test-port*))
+        (client2 (open-connection *localhost* *test-port*)))
+    (let ((server1 (accept-connection passive-socket))
+          (server2 (accept-connection passive-socket))
+          (form '(:a 1 :b "2" :c 3.0 :d 't)))
+      (close-passive-socket passive-socket)
+      ;; Print test form:
+      (print form client1)
+      ;; Print it again:
+      (print form client1)
+      (force-output client1)
+      ;; Read test form:
+      (let ((read-form (read server1)))
+        (unless (equal form read-form)
+          (log-error "Wrong form read: ~s" read-form)))
+      ;; Read it again:
+      (let ((read-form (read server1)))
+        (unless (equal form read-form)
+          (log-error "Wrong form read: ~s" read-form)))
+      (close client1)
+      (close server1)
+      ;; Shutdown client2:
+      (shutdown-socket-stream client2 ':output)
+      (let ((read-form (read server2 nil ':eof)))
+        (unless (eq read-form ':eof)
+          (log-error "Didn't receive client shutdown")))
+      (close server2)
+      (let ((read-form (read client2 nil ':eof)))
+        (unless (eq read-form ':eof)
+          (log-error "Didn't receive server close")))
+      (close client2)))
+  (forced-format "~&;; Passive-socket and connection tests completed."))
+
+;;; ---------------------------------------------------------------------------
+
+(defun portable-sockets-test ()
+  (http-test "GBBopen.org" 80)
+  (passive-socket-tests))
+
+;;; ---------------------------------------------------------------------------
+
+(when *autorun-modules* (portable-sockets-test))
 
 ;;; ===========================================================================
 ;;;				  End of File
