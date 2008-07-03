a66ee66e5e59d6d71cd009bbf02704ff4abd0cb1
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2008-07-03 16:51:32 +0000

    Add more syntactic-sugar (equivalent to fixnum) pseudo-probability operators

--- source/tools/pseudo-probabilities.lisp
+++ source/tools/pseudo-probabilities.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-TOOLS; Syntax:common-lisp -*-
 ;;;; *-* File: /usr/local/gbbopen/source/tools/pseudo-probabilities.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Thu Jul  3 02:44:23 2008 *-*
+;;;; *-* Last-Edit: Thu Jul  3 12:50:35 2008 *-*
 ;;;; *-* Machine: cyclone.cs.umass.edu *-*
 
 ;;;; **************************************************************************
@@ -26,10 +26,9 @@
 (in-package :gbbopen-tools)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (export '(+%
-            -%
-            *%
-            /%
+  ;; Generated fixnum-equivalent operators are exported by DEFDN% (below):
+  (export '(*%                          ; normalizing *&
+            /%                          ; normalizing /&
             exp%
             pprob2prob
             prob2pprob
@@ -39,6 +38,17 @@
 ;;; ===========================================================================
 ;;;   Pseudo-Probabilities
 ;;;
+;;;  A "pseudo probability" represents a [0.0..1.0] value as a fixnum between
+;;;  0 and MAX-PPROB (normally [0..1000]).  This provides fast, no allocation,
+;;;  computations with discretized probability values.
+;;;
+;;;  CLISP provides the fewest fixnum bits (24).  In order to keep the value
+;;;  of (ln% 1) a fixnum we use 1000 as MAX-PPROB.  (1513 is the largest
+;;;  MAX-PPROB value where (ln% 1) can be represented as a 24-bit fixnum, but
+;;;  we want to keep MAX-PPROB a power of ten to allow easy visual
+;;;  transposition of the decimal point for human translation between real and
+;;;  pseudo-probability values.  7743 is the largest MAX-PPROB value where (ln% 1)
+;;;  is a 29-bit fixnum.)  
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defconstant max-pprob 1000)
@@ -63,21 +73,54 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(defun +% (&rest args)
-  (declare (dynamic-extent args0))
-  (apply #'+ args))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmacro defdn% (op &optional result values-types)
+    (declare (ignore result values-types))
+    (let ((pprob-op (intern (concatenate 'simple-string
+                              (symbol-name op) "%")
+                            ':gbbopen-tools))
+          (fixnum-op (intern (concatenate 'simple-string
+                               (symbol-name op) "&")
+                             ':gbbopen-tools)))
+      `(progn
+         (eval-when (:compile-toplevel :load-toplevel :execute)
+           (export '(,pprob-op)))
+         
+         (defun ,pprob-op (&rest args)
+           (declare (dynamic-extent args0))
+           (apply #',fixnum-op args))
+         
+         (defcm ,pprob-op (&rest args)
+           `(,',fixnum-op ,@args))))))
 
-(defcm +% (&rest args)
-  `(+& ,@args))
-
-;;; ---------------------------------------------------------------------------
-
-(defun -% (&rest args)
-  (declare (dynamic-extent args0))
-  (apply #'- args))
-
-(defcm -% (&rest args)
-  `(-& ,@args))
+(defdn% + t)
+(defdn% 1+  t)
+(defdn% - t)
+(defdn% 1- t)
+(defdn% =)
+(defdn% /=)
+(defdn% <)
+(defdn% <=)
+(defdn% >)
+(defdn% >=)
+(defdn% min t)
+(defdn% max t)
+(defdn% zerop)
+(defdn% plusp)
+(defdn% minusp)
+(defdn% evenp)
+(defdn% oddp)
+(defdn% abs t)
+;; Not yet available:
+;; (defdn% mod t)
+;; (defdn% floor t (fixnum fixnum))
+;; (defdn% ceiling t (fixnum fixnum))
+;; (defdn% truncate t (fixnum fixnum))
+;; (defdn% round t (fixnum fixnum))
+;; (defdn% ffloor t (float fixnum))
+;; (defdn% fceiling t (float fixnum))
+;; (defdn% ftruncate t (float fixnum))
+;; (defdn% fround t (float fixnum))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -121,23 +164,18 @@
 (defcm /%-2op (pprob1 pprob2)
   `(truncate& (*& ,pprob1 ,max-pprob) ,pprob2))
 
-(defun unary-/%-error (pprob1)
-  (error "Unary /% can only be called with ~s, ~s was provided."
-         max-pprob
-         pprob1))
-
 (defun /% (pprob1 &rest args)
   (declare (dynamic-extent args))
-  (cond 
-   ;; not unary:
-   (args
-    (let ((result pprob1))
-      (while args
-        (setf result (/%-2op result (pop args))))
-      result))
-   ;; unary doesn't really make sense, as only max-pprob is legal:
-   ((=& max-pprob pprob1) max-pprob)
-   (t (unary-/%-error pprob1))))
+  (if args
+      ;; not unary:
+      (let ((result pprob1))
+        (while args
+          (setf result (/%-2op result (pop args))))
+        result)
+      ;; Unary /% doesn't really make sense, as only (/ MAX-PPROB) returns a
+      ;; legal pseudo-probabilty value, but we'll give the user some rope to
+      ;; hang themselves with:
+      (/%-2op max-pprob pprob1)))
 
 (defcm /% (first-arg &rest args)
   (if args
@@ -152,27 +190,24 @@
                            (first args))))
                   (another args)))
       ;; one arg:
-      (with-once-only-bindings (first-arg)
-        `(if (=& ,max-pprob ,first-arg)
-             ,max-pprob
-             (unary-/%-error ,first-arg)))))
+      `(/%-2op max-pprob ,first-arg)))
 
 ;;; ---------------------------------------------------------------------------
 
 (defvar *ln%-vector* 
     ;; The ln% lookup table
-    (let ((vector (make-array (list (1+& max-pprob))))
-          (multiplier (float max-pprob)))
+    (let ((vector (make-array (list (1+& max-pprob)))))
       (loop for i fixnum from 1 to max-pprob
           do (setf (svref vector i) 
                    (prob2pprob
-                    (*$ multiplier (log (pprob2prob i))))))
+                    (*$ max-pprob$ (log (pprob2prob i))))))
       vector))
 
 ;;; ---------------------------------------------------------------------------
 
 (defun ln%-error (pprob)
-  (error "Unable to take the ln% of ~s"
+  (error "Unable to take the ~s of ~s"
+         'ln%         
          (/$ (float pprob) #.(float max-pprob))))
 
 (defun ln% (pprob)
@@ -194,7 +229,8 @@
   ;;; Return e**pprob-ln where pprob-ln is a fixnum between (ln% 1) and 0
   (with-full-optimization ()
   (unless (<=& (load-time-value (ln% 1)) pprob-ln 0)
-    (error "Unable to represent exp% of ~s as a pseudo-probability"
+    (error "Unable to represent the ~s of ~s as a pseudo-probability"
+           'exp%
            pprob-ln))
   ;; Perform reverse lookup in *ln%-vector* using binary search:
   (let ((min 1)
