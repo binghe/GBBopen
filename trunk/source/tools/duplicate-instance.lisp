df78ddca98770921071993761815ecfa795a2be7
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2009-09-28 09:35:36 +0000

    Rework MAKE-DUPLICATE-INSTANCE and MAKE-DUPLICATE-INSTANCE-CHANGING-CLASS to eliminate the need for UNBOUND-VALUE-INDICATOR escaping

--- trunk/source/tools/duplicate-instance.lisp
+++ trunk/source/tools/duplicate-instance.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-TOOLS; Syntax:common-lisp -*-
 ;;;; *-* File: /usr/local/gbbopen/source/tools/duplicate-instance.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Sun Sep 27 06:26:37 2009 *-*
+;;;; *-* Last-Edit: Mon Sep 28 04:39:30 2009 *-*
 ;;;; *-* Machine: cyclone.cs.umass.edu *-*
 
 ;;;; **************************************************************************
@@ -39,7 +39,7 @@
                                                     unduplicated-slot-names
                                                     &rest initargs))
 (defgeneric initialize-duplicated-instance (instance slots slot-values 
-                                            missing-slot-names))
+                                            slot-names-to-initialize))
 (defgeneric unduplicated-slot-names (instance))
 
 ;;; ---------------------------------------------------------------------------
@@ -51,27 +51,22 @@
 
 (defmethod initialize-duplicated-instance ((instance standard-object)
                                            slots slot-values 
-                                           missing-slot-names)
+                                           slot-names-to-initialize)
   (let ((class (class-of instance)))
     (loop for slot in slots
-        and value in slot-values
-        do (when slot
-             ;; Set the slot value, unless it is to remain unbound:
-             (unless (eq value unbound-value-indicator)
-               ;; Handle the UNBOUND-VALUE-INDICATOR escape:
-               (when (eq value the-unbound-value-indicator)
-                 (setf value unbound-value-indicator))
-               ;; Lispworks can't use slot-value-using-class here
-               ;; (prior to shared-initialize?):
-               #+lispworks
-               (setf (slot-value instance (slot-definition-name slot)) value)
-               #-lispworks
-               (setf (slot-value-using-class class instance slot) 
-                     value))))
-    ;; Initialize any remaining slots:
-    (when missing-slot-names
-      (let ((*%%skip-gbbopen-shared-initialize-method-processing%%* 't))
-        (shared-initialize instance missing-slot-names)))))
+        and value in slot-values do 
+          ;; Lispworks can't use slot-value-using-class here
+          ;; (prior to shared-initialize?):
+          #+lispworks
+          (setf (slot-value (slot-definition-name slot) slot-name) value)
+          #-lispworks
+          (setf (slot-value-using-class class instance slot) 
+                value)))
+  ;; Initialize any missing slots (but not existing slots that are to remain
+  ;; unbound):
+  (when slot-names-to-initialize
+    (let ((*%%skip-gbbopen-shared-initialize-method-processing%%* 't))
+      (shared-initialize instance slot-names-to-initialize))))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -93,10 +88,10 @@
          (new-instance (allocate-instance new-class))
          (slots nil)
          (slot-values nil)
-         (missing-slot-names nil)
+         (slot-names-to-initialize nil)
          (computed-unduplicated-slot-names (unduplicated-slot-names instance))
-         (not-found-value ""))
-    (declare (dynamic-extent slots slot-values missing-slot-names))
+         (not-found-value '#.(gensym)))
+    (declare (dynamic-extent slots slot-values slot-names-to-initialize))
     (dolist (slot (class-slots new-class))
       (let* ((slot-name (slot-definition-name slot))
              (old-slot (find slot-name old-class-slots :key #'slot-definition-name))
@@ -109,27 +104,23 @@
          ((not (eq maybe-initarg-value not-found-value))
           (push slot slots)
           (push maybe-initarg-value slot-values))
-         ;; Record as a missing-slot-name, if asked not to duplicate it
+         ;; Record as a slot-name-to-initialize, if asked not to duplicate it
          ;; (either specified in `unduplicated-slot-names' or computed by
          ;; UNDUPLICATED-SLOT-NAMES method) or if this slot is defined only in
          ;; the new class:
          ((or (not old-slot)
               (memq slot-name computed-unduplicated-slot-names)
               (memq slot-name unduplicated-slot-names))
-          (push slot-name missing-slot-names))
-         ;; Not an initarg or an unduplicated-slot-name, use the slot-value
-         ;; from the old instance:
+          (push slot-name slot-names-to-initialize))
+         ;; Slot is unbound (and should stay so in the duplicated instance):
+         ((not (slot-boundp-using-class old-class instance old-slot)))
+         ;; Not an initarg or an unduplicated-slot-name or an unbound slot,
+         ;; use the slot-value from the old instance:
          (t (push slot slots)
-            (push (if (slot-boundp-using-class old-class instance old-slot)
-                      (let ((value (slot-value-using-class 
-                                    old-class instance old-slot)))
-                        (if (eq value unbound-value-indicator)
-                            the-unbound-value-indicator
-                            value))
-                      unbound-value-indicator)
+            (push (slot-value-using-class old-class instance old-slot)
                   slot-values)))))
     (initialize-duplicated-instance 
-     new-instance slots slot-values missing-slot-names)
+     new-instance slots slot-values slot-names-to-initialize)
     (values new-instance slots)))
 
 ;;; ---------------------------------------------------------------------------
