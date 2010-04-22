85dfdd77256663446d72f66860ee61971ac22e76
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2010-04-22 12:44:42 +0000

    CCL 1.5 transition sizes

--- trunk/source/tools/timing/cl-timing.lisp
+++ trunk/source/tools/timing/cl-timing.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:GBBOPEN-TOOLS-USER; Syntax:common-lisp -*-
 ;;;; *-* File: /usr/local/gbbopen/source/tools/timing/cl-timing.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Tue Apr 20 04:51:49 2010 *-*
+;;;; *-* Last-Edit: Tue Apr 20 04:57:55 2010 *-*
 ;;;; *-* Machine: cyclone.cs.umass.edu *-*
 
 ;;;; **************************************************************************
@@ -682,7 +682,7 @@
            (let ((computed-transition
                   (funcall compute-fn
                            ;; Start timing slightly below the specified size:
-                           (-& size (max& 3 (round (* size 0.1))))
+                           (-& size (max& 3 (round (* size 0.2))))
                            verbose?)))
              ;; Does the specified transition size appear too high?  Recompute
              ;; it from size 1:
