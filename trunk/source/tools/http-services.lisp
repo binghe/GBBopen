32d6e4d7298c80d48ed86d1d10073bfad8898399
cork <cork@5f49af62-2c63-493f-87c0-414fe00f4c69>
2010-06-21 17:55:13 +0000

    Changes to currently undocumented HTTP-service entities

--- trunk/source/tools/http-services.lisp
+++ trunk/source/tools/http-services.lisp
@@ -1,7 +1,7 @@
 ;;;; -*- Mode:Common-Lisp; Package:HTTP-SERVICES; Syntax:common-lisp -*-
 ;;;; *-* File: /usr/local/gbbopen/source/tools/http-services.lisp *-*
 ;;;; *-* Edited-By: cork *-*
-;;;; *-* Last-Edit: Sun May 16 11:55:38 2010 *-*
+;;;; *-* Last-Edit: Mon Jun 21 13:31:01 2010 *-*
 ;;;; *-* Machine: cyclone.cs.umass.edu *-*
 
 ;;;; **************************************************************************
@@ -39,30 +39,31 @@
 (in-package :http-services)
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (export '(*http-server-port*          ; not yet documented
-            *log-http-connections*      ; not yet documented
-            *log-http-headers*          ; not yet documented
-            *log-http-requests*         ; not yet documented
-            close-http-connection       ; not yet documented
+  (export '(*http-server*               ; not yet documented
             decode-uri-string           ; not yet documented
             encode-xml-string           ; not yet documented
             encode-xml-value            ; not yet documented
             handle-http-get             ; not yet documented
             kill-http-server            ; not yet documented
+            make-http-server            ; not yet documented
             send-http-response-headers  ; not yet documented
             start-http-server           ; not yet documented
             write-crlf)))               ; not yet documented
 
- ;;; ---------------------------------------------------------------------------
+;;; ---------------------------------------------------------------------------
 
-(defvar *http-server-port* 8052)
-(defvar *log-http-connections* 't)
-(defvar *log-http-headers* nil)
-(defvar *log-http-requests* 't)
-(defvar *http-server-thread* nil)
-(defvar *http-clients* nil)             ; none yet...
-(defvar *http-log-stream* *standard-output*)
-(defvar *http-log-stream-lock* (make-lock :name "HTTP log stream"))
+(defstruct (http-server (:copier nil))
+  port
+  thread
+  clients
+  (name "HTTP Connection Server")
+  (log-stream *standard-output*)
+  (log-stream-lock (make-lock :name "HTTP log stream"))
+  log-connections
+  log-headers
+  log-requests)
+  
+(defvar *http-server*)
 
 ;;; ---------------------------------------------------------------------------
 
@@ -140,22 +141,24 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(defun write-to-http-log (control-string &rest args)
-  (when *http-log-stream*
-    (with-lock-held (*http-log-stream-lock*)
-      (format *http-log-stream* "~&;; ~a ~?~%"
-              (message-log-date-and-time)
-              control-string
-              args))))
+(defun write-to-http-log (server control-string &rest args)
+  (let ((log-stream (http-server-log-stream server)))
+    (when log-stream
+      (with-lock-held ((http-server-log-stream-lock server))
+        (format log-stream "~&;; ~a ~?~%"
+                (message-log-date-and-time)
+                control-string
+                args)))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun add-to-http-log (control-string &rest args)
-  (when *http-log-stream*
-    (with-lock-held (*http-log-stream-lock*)
-      (format *http-log-stream* "~&;;~19t~?~%"
-              control-string
-              args))))
+(defun add-to-http-log (server control-string &rest args)
+  (let ((log-stream (http-server-log-stream server)))
+    (when log-stream
+      (with-lock-held ((http-server-log-stream-lock server))
+        (format log-stream "~&;;~19t~?~%"
+                control-string
+                args)))))
 
 ;;; ---------------------------------------------------------------------------
 
@@ -204,7 +207,7 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(defun read-remaining-http-request-headers (connection)
+(defun read-remaining-http-request-headers (server connection)
   ;; Reads the HTTP request headers (after the inital request header),
   ;; returning user-agent and connection-type values, if found:
   (let ((user-agent nil)
@@ -212,7 +215,8 @@
   (loop
       for line = (read-http-line connection)
       until (zerop (length line)) do
-        (when *log-http-headers* (add-to-http-log "~s" line))
+        (when (http-server-log-headers server)
+          (add-to-http-log server "~s" line))
         (cond
          ;; User-Agent:
          ((and (>& (length line) 12) (string= "User-Agent: " line :end2 12))
@@ -224,25 +228,28 @@
 
 ;;; ---------------------------------------------------------------------------
 
-(defun close-http-connection (connection) 
+(defun close-http-connection (server connection)
   (when (open-stream-p connection)
-    (when *log-http-connections*
+    (when (http-server-log-connections server)
       (multiple-value-call 'write-to-http-log 
+        server
         "Closing connection from ~a/~s"
         (remote-hostname-and-port connection)))
     (close connection)))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun http-connection-thread (connection)
-    (when *log-http-connections*
-      (multiple-value-call 'write-to-http-log 
-        "Connection from ~a/~s"
-        (remote-hostname-and-port connection)))
+(defun http-connection-thread (server connection)
+  (when (http-server-log-connections server)
+    (multiple-value-call 'write-to-http-log 
+      server
+      "Connection from ~a/~s"
+      (remote-hostname-and-port connection)))
   (loop
     (let ((line (read-http-line connection)))
-      (when (and line (or *log-http-requests* *log-http-headers*))
-        (add-to-http-log "~s" line))
+      (when (and line (or (http-server-log-requests server)
+                          (http-server-log-headers server)))
+        (add-to-http-log server "~s" line))
       (cond
        ;; The line is dead:
        ((not line) (return))
@@ -250,7 +257,7 @@
        ((and (>& (length line) 3) (string= "GET" line :end2 3))
         (let ((path (subseq line 4 (position #\Space line :start 5 :test #'eq))))
           (multiple-value-bind (user-agent connection-type)
-              (read-remaining-http-request-headers connection)
+              (read-remaining-http-request-headers server connection)
             (funcall 'handle-http-get connection path user-agent)
             (unless (string= connection-type "keep-alive")
               (return)))))
@@ -262,31 +269,39 @@
             (unless (string= connection-type "keep-alive")
               (return))))))
     (force-output connection))
-  (close-http-connection connection))
-  
+  (close-http-connection server connection))
+
 ;;; ---------------------------------------------------------------------------
 
 (defun http-connection-server (connection) 
-  (spawn-thread "HTTP Connection" 'http-connection-thread connection))
+  (spawn-thread "HTTP Connection" 'http-connection-thread
+                ;; Grab the server from the global binding (typically bound in
+                ;; START-HTTP-SERVER):
+                *http-server*
+                connection))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun start-http-server (&optional (port *http-server-port*))
-  (setf *http-server-thread*
-        (start-connection-server 'http-connection-server
-                                 port
-                                 :name "HTTP Connection Server"
-                                 :reuse-address 't)))
+(defun start-http-server (&optional (*http-server* *http-server*))
+  (let ((server *http-server*))
+    (setf (http-server-thread server)
+          (start-connection-server 'http-connection-server
+                                   (http-server-port server)
+                                   :name (http-server-name server)
+                                   :reuse-address 't))))
 
 ;;; ---------------------------------------------------------------------------
 
-(defun kill-http-server (&optional (kill-clients 't))
-  (when *http-server-thread*
-    (kill-thread *http-server-thread*))
-  (when kill-clients
-    (dolist (client *http-clients*)
-      (kill-thread client))
-    (setf *http-clients* nil)))
+(defun kill-http-server (&optional (server *http-server*)
+                                   (kill-clients 't))
+  (let ((server-thread (http-server-thread server)))
+    (when server-thread
+      (kill-thread server-thread)
+      (setf (http-server-thread server) nil))
+    (when kill-clients
+      (dolist (client (http-server-clients server))
+        (kill-thread client))
+      (setf (http-server-clients server) nil))))
 
 ;;; ===========================================================================
 ;;;				  End of File
